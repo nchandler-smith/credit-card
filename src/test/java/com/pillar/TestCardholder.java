b3ba047ae0253248381bd64fca9b8074616b4de9
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-12-03 12:55:33 -0600

    failing test if two card holder ids are not equal

--- src/test/java/com/pillar/TestCardholder.java
+++ src/test/java/com/pillar/TestCardholder.java
@@ -4,6 +4,7 @@ import com.pillar.cardholder.Cardholder;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TestCardholder {
 
@@ -13,5 +14,13 @@ public class TestCardholder {
         Cardholder cardholder2 = new Cardholder(1);
 
         assertEquals(cardholder1, cardholder2);
+    }
+
+    @Test
+    public void twoCardholderAreNotEqualIfTheirIdsAreNotIdentical() {
+        Cardholder cardholder1 = new Cardholder(1);
+        Cardholder cardholder2 = new Cardholder(2);
+
+        assertNotEquals(cardholder1, cardholder2);
     }
 }
