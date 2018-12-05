dd256269f5fbb929aa212082240dcfa3b4dff48b
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-12-05 08:29:39 -0600

    refactoring tests

--- src/test/java/com/pillar/TestAccount.java
+++ src/test/java/com/pillar/TestAccount.java
@@ -1,16 +1,22 @@
 package com.pillar;
 
 import com.pillar.account.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class TestAccount {
+    Account account1;
+
+    @Before
+    public void setuo() {
+        account1 = new Account(1);
+    }
 
     @Test
     public void twoAccountsAreEqualIfTheirIdsAreEqual() {
-        Account account1 = new Account(1);
         Account account2 = new Account(1);
 
         assertEquals(account1, account2);
@@ -18,7 +24,6 @@ public class TestAccount {
 
     @Test
     public void twoAccountsAreNotEqualIfTheirIdsAreNotEqual() {
-        Account account1 = new Account(1);
         Account account2 = new Account(2);
 
         assertNotEquals(account1, account2);
