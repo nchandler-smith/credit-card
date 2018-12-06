f4dd62121aa03ac1c982f6582cb6629403a2278a
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-12-06 08:31:52 -0600

    account API controller is not null

--- src/test/java/com/pillar/TestAccountApiController.java
+++ src/test/java/com/pillar/TestAccountApiController.java
@@ -1,4 +1,15 @@
 package com.pillar;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class TestAccountApiController {
+
+    @Test
+    public void accountApiCotrollerExists() {
+        AccountApiController controller = new AccountApiController();
+
+        assertNotNull(controller);
+    }
 }
