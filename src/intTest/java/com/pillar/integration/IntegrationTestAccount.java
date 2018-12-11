d48bf1e5104a286557716e9a5166c975bd743afb
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-12-10 21:02:22 -0600

    WIP: adding integration tests for account API controller

--- src/intTest/java/com/pillar/integration/IntegrationTestAccount.java
+++ src/intTest/java/com/pillar/integration/IntegrationTestAccount.java
@@ -1,4 +1,23 @@
 package com.pillar.integration;
 
+import com.pillar.cardholder.Cardholder;
+import com.pillar.cardholder.CardholderRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegrationTestAccount {
+
+    @Autowired
+    private CardholderRepository cardholderRepository;
+
+    @Test
+    public void testEmptyCardholderTableHasNoRecords() {
+        List<Cardholder> cardholders = cardholderRepository.findAll();
+
+        assertEquals(0, cardholders.size());
+    }
 }
--- src/main/java/com/pillar/AccountApiController.java
+++ src/main/java/com/pillar/AccountApiController.java
@@ -2,8 +2,18 @@ package com.pillar;
 
 import com.pillar.account.Account;
 import com.pillar.cardholder.Cardholder;
+import com.pillar.cardholder.CardholderRepository;
+
+import java.util.List;
 
 public class AccountApiController {
+    private CardholderRepository cardholderRepository;
+
+    public AccountApiController(CardholderRepository cardholderRepository) {
+        this.cardholderRepository = cardholderRepository;
+    }
+
+    public List<Cardholder> getAll() { return cardholderRepository.findAll(); }
 
     public Account createAccount(Cardholder cardholder){
         Account account = new Account(cardholder);
