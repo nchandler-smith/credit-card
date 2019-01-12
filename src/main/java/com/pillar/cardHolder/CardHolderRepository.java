c86ed34ca44684bb5c9e5c08e5aba744a1b0d85e
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-01-12 14:50:18 -0600

    removed unneeded file

--- src/main/java/com/pillar/cardHolder/CardHolderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.pillar.cardHolder;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardHolderRepository extends JpaRepository<CardHolder, Integer> {
-}
