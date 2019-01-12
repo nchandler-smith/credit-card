adc60a160c51b3c50ff323334807a8b36d64f7cd
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-01-12 14:10:41 -0600

    card holder initial commit card holder has name

---
+++ src/main/java/com/pillar/cardHolder/CardHolder.java
@@ -0,0 +1,7 @@
+package com.pillar.cardHolder;
+
+public class CardHolder {
+    private String name = "Steve Goliath";
+
+    public String getName() { return this.name; }
+}
---
+++ src/test/java/com/pillar/cardHolderTests/TestCardHolder.java
@@ -0,0 +1,16 @@
+package com.pillar.cardHolderTests;
+
+import com.pillar.cardHolder.CardHolder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestCardHolder {
+
+    @Test
+    public void aCardHolderNameIsNotNull() {
+        CardHolder cardHolder = new CardHolder();
+
+        assertNotNull(cardHolder.getName());
+    }
+}
