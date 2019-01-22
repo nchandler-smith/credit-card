8d0d9421d39d07212ac8e905adc3733eecbc0c6c
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-01-21 20:24:06 -0600

    empty table returns nothing

--- src/main/java/com/pillar/account/Account.java
+++ src/main/java/com/pillar/account/Account.java
@@ -1,9 +1,13 @@
 package com.pillar.account;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
+@Entity
 public class Account {
 
+    @Id
     private final Integer id = 1;
     private String cardNumber;
     private final Double creditLimit = 10000.0;
